21f2f88999f931d8054f3efcfdf2fb070170ce85
Hammerslay <hammerslay@gmail.com>
2017-10-15 13:33:11 +0200

    Updated model.

--- StudentLife/StudentLife/Model/Hap.cs
+++ StudentLife/StudentLife/Model/Hap.cs
@@ -8,5 +8,22 @@ namespace StudentLife.Model
 {
     class Hap
     {
+        private int id;
+        private string name;
+        private DateTime hapDate; //Might only need two of these three DateTimes, one for start and one for end?
+        private DateTime startTime;
+        private DateTime endTime;
+        private string hapDescription;
+        private int cost;
+        private Organization orgName;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public DateTime HapDate { get => hapDate; set => hapDate = value; }
+        public DateTime StartTime { get => startTime; set => startTime = value; }
+        public DateTime EndTime { get => endTime; set => endTime = value; }
+        public string HapDescription { get => hapDescription; set => hapDescription = value; }
+        public int Cost { get => cost; set => cost = value; }
+        public Organization OrgName { get => orgName; set => orgName = value; }
     }
 }
--- StudentLife/StudentLife/Model/Organization.cs
+++ StudentLife/StudentLife/Model/Organization.cs
@@ -8,6 +8,20 @@ namespace StudentLife.Model
 {
     class Organization
     {
-        //Test
+        private string name;
+        private string adress;
+        private string phoneNumber;
+        private string email;
+        private string website;
+        private string orgDescription;
+        private List<Hap> haps;
+
+        public string Name { get => name; set => name = value; }
+        public string Adress { get => adress; set => adress = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string Email { get => email; set => email = value; }
+        public string Website { get => website; set => website = value; }
+        public string OrgDescription { get => orgDescription; set => orgDescription = value; }
+        public List<Hap> Haps { get => haps; set => haps = value; }
     }
 }
