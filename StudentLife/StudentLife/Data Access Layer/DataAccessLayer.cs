4070728baec669ae3f0e701cd10210d0bbaa75d1
Hammerslay <hammerslay@gmail.com>
2017-10-15 11:23:09 +0200

    Added files for exception handling and for model classes.

--- StudentLife/StudentLife/Data Access Layer/DataAccessLayer.cs
+++ StudentLife/StudentLife/Data Access Layer/DataAccessLayer.cs
@@ -12,28 +12,35 @@ namespace StudentLife.Data_Access_Layer
     {
         static void Test()
         {
-            using (SqlConnection conn = new SqlConnection("Server=localhost; Database=StudentLife; Integrated Security=true"))
+            try
             {
-                conn.Open();
-
-                //SqlCommand cmd = new SqlCommand("SELECT * FROM Organization;", conn);
-                //SqlDataReader reader = cmd.ExecuteReader();
-                //while (reader.Read())
-                //{
-                //    Console.WriteLine(reader.GetString(0));
-                //}
-                //Console.ReadLine();
-                //conn.Close()
-
-                SqlCommand cmd = new SqlCommand("usp_GetAllOrganizations", conn);
-                cmd.CommandType = CommandType.StoredProcedure;
-                //cmd.Parameters.Add(new SqlParameter("@name", "Helsingkrona"));
-                SqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
+                using (SqlConnection conn = new SqlConnection("Server=localhost; Database=StudentLife; Integrated Security=true"))
                 {
-                    Console.WriteLine(reader.GetString(4) + reader.GetString(1) + reader.GetString(2) + reader.GetString(3) + reader.GetString(4) + reader.GetString(5));
+                    conn.Open();
+
+                    //SqlCommand cmd = new SqlCommand("SELECT * FROM Organization;", conn);
+                    //SqlDataReader reader = cmd.ExecuteReader();
+                    //while (reader.Read())
+                    //{
+                    //    Console.WriteLine(reader.GetString(0));
+                    //}
+                    //Console.ReadLine();
+                    //conn.Close()
+
+                    SqlCommand cmd = new SqlCommand("usp_GetAllOrganizations", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    //cmd.Parameters.Add(new SqlParameter("@name", "Helsingkrona"));
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader.GetString(4) + reader.GetString(1) + reader.GetString(2) + reader.GetString(3) + reader.GetString(4) + reader.GetString(5));
+                    }
+                    Console.ReadLine();
                 }
-                Console.ReadLine();
+            }
+            catch
+            {
+
             }
         }
     }
---
+++ StudentLife/StudentLife/Exception Handling/ExceptionHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLife.Exception_Handling
+{
+    class ExceptionHandler
+    {
+        void HandleException (Exception ex)
+        {
+            switch (ex.GetType())
+            {
+
+            }
+        }
+    }
+}
